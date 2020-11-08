6c01d59786c60b9e612be206f7da769748a5f8a3
pparkst@naver.com <ppark12@funshop.co.kr>
2020-11-08 21:49:39 +0900

    init : Gradle Jpa Mysql REST API

--- SeoulDataAPI/database/dbcon.py
+++ SeoulDataAPI/database/dbcon.py
@@ -1,6 +1,6 @@
-import MySQLdb
+from MySQLdb import _mysql
 
-db = MySQLdb.connect("????")
+db = _mysql.connect("rds-mysql-8v.cej8zsslhqnj.ap-northeast-2.rds.amazonaws.com:44433", "sa", "wjdals1qaz!", "pparkst_db")
 cur = db.cursor()
 cur.execute("SELECT sysdate()")
 res = cur.fetchall()
